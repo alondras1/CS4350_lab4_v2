4f6c20bac0be15ba15794ecaf38dbedc2d39f452
Alondra Sanchez <89600410+alondras1@users.noreply.github.com>
2022-04-26 15:40:17 -0700

    connection to mysql works

--- src/project1/Main.java
+++ src/project1/Main.java
@@ -1,22 +1,32 @@
 package project1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
+    private static Statement mystatement;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);  // Create a Scanner object
 
-//        String conURL = "jdbc:mysql://localhost:3306/cs4350_lab4";
-//        String user = "root";
-//        //          SQL statements
-//        String display_bus = "SELECT * FROM Bus";
+        String conURL = "jdbc:mysql://localhost:3306/cs4350_lab4";
+        String user = "root";
+        String password = "Pemd@sislife!12";
+        try {
+            // get connection
+            Connection myConn = DriverManager.getConnection(conURL, user, password);
+            // create statement
+            mystatement = myConn.createStatement();
+//            ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
 //
-//        make_connection();
+//            while(myres.next()){
+//                System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+//            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
 
+//        user selection
         int user_selection;
 
         do {
@@ -32,70 +42,56 @@ public class Main {
             switch (user_selection){
                 case 1:
 //                    call display function
-                    displaySchedule();
+                    displaySchedule(mystatement);
                     break;
                 case 2:
 //                    delete a trip
-                    deleteTripOffering();
+                    deleteTripOffering(mystatement);
                     break;
                 case 3:
 //                    Add a Trip Offering
-                    addTripOffering();
+                    addTripOffering(mystatement);
                     break;
                 case 4:
 //                    Change a Driver
-                    changeDriver();
+                    changeDriver(mystatement);
                     break;
                 case 5:
 //                    Change a Bus
-                    changeBus();
+                    changeBus(mystatement);
                     break;
                 case 6:
 //                    Display Trip Stops
-                    displayTripStops();
+                    displayTripStops(mystatement);
                     break;
                 case 7:
 //                    Display Weekly Schedule for Driver
-                    dispWeeklySchedule();
+                    dispWeeklySchedule(mystatement);
                     break;
                 case 8:
 //                    Add a Driver
-                    addDriver();
+                    addDriver(mystatement);
                     break;
                 case 9:
 //                    Add a Bus
-                    addBus();
+                    addBus(mystatement);
                     break;
                 case 10:
 //                    Delete a Bus
-                    deleteBus();
+                    deleteBus(mystatement);
                     break;
                 case 11:
 //                    Insert Actual Trip Info
-                    insert_Actual_TripInfo();
+                    insert_Actual_TripInfo(mystatement);
                     break;
             }
 
         }while (user_selection != 0);
     }
 
-    private static void make_connection() {
-        String conURL = "jdbc:mysql://localhost:3306/cs4350_lab4";
-        String user = "root";
-        String password = "Pemd@sislife!12";
-        try {
+    private static Statement make_connection() {
 
-            // get connection
-            Connection myConn = DriverManager.getConnection(conURL, user, password);
-
-            // create statement
-            Statement mystatement = myConn.createStatement();
-
-        } catch (Exception exc) {
-            exc.printStackTrace();
-
-
-        }
+        return null;
     }
 
     private static void menu(){
@@ -110,7 +106,7 @@ public class Main {
                 +"9:\tAdd a Bus\n"
                 +"10:\tDelete a Bus\n"
                 +"11:\tInsert Actual Trip Info\n"
-                +"x:\tExit program\n"
+                +"0:\tExit program\n"
         );
     }
 
@@ -119,66 +115,126 @@ public class Main {
      * and Date
      * addition to these attributes, the schedule includes: Scheduled StartTime,ScheduledArrivalTime , DriverID, and BusID
      * */
-    public static void displaySchedule(){}
+    public static void displaySchedule(Statement mystatement) throws SQLException{
+
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
     /**
      * 2. Delete a trip offering specified by Trip#, Date, and ScheduledStartTime
      * */
-    public static void deleteTripOffering(){}
+    public static void deleteTripOffering(Statement mystatement) throws SQLException {
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
     /**
      * 3. Add a set of trip offerings assuming the values of all attributes are given (the software
      * asks if you have more trips to enter)
      * */
-    public static void addTripOffering(){}
+    public static void addTripOffering(Statement mystatement) throws SQLException{
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
     /**
      * 4. Change the driver for a given Trip offering (i.e given TripNumber, Date,
      * ScheduledStartTime)
      * */
-    public static void changeDriver(){}
+    public static void changeDriver(Statement mystatement) throws SQLException{
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
     /**
      * 5. Change the bus for a given Trip offering
      * */
-    public static void changeBus(){
-        String oldBusID = "";
-        String newBusID = "";
-        String tripOffering = "";
-        System.out.println("Please provide the TripID whose bus you'd like to change: ");
+    public static void changeBus(Statement mystatement) throws SQLException{
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
 
-        System.out.println("Please provide the new BusID: ");
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
     }
 
     /**
      * 6. Display the stops of a given trip ( i.e. the attributes of the table TripStopInfo)
      * */
-    public static void displayTripStops(){}
+    public static void displayTripStops(Statement mystatement) throws SQLException{
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
     /**
      * 7. Display the weekly schedule of a given driver and date
      * */
-    private static void dispWeeklySchedule(){}
+    private static void dispWeeklySchedule(Statement mystatement) throws SQLException{
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
     /**
      * 8. Add a driver
      * */
-    private static void addDriver(){}
+    private static void addDriver(Statement mystatement) throws SQLException{
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
     /**
      * 9. Add a bus
      * */
-    private static void addBus(){}
+    private static void addBus(Statement mystatement) throws SQLException{
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
     /**
      * 10. Delete a bus
      * */
-    private static void deleteBus(){}
+    private static void deleteBus(Statement mystatement) throws SQLException{
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
     /**
      * 11. Record (insert) the actual data of a given trip offering specified by its key. The actual
      * data include the attributes of the table ActualTripStopInfo
      * */
-    private static void insert_Actual_TripInfo(){}
+    private static void insert_Actual_TripInfo(Statement mystatement) throws SQLException{
+        ResultSet myres = mystatement.executeQuery("Select * FROM Driver");
+
+        while(myres.next()){
+            System.out.println(myres.getString("DriverName") + myres.getString("DriverTelephoneNumber"));
+        }
+    }
 
 }
